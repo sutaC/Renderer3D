e202cfc751b90b7461eac7ef21fbe4687d77ce07
sutaC <catus.pl@gmail.com>
2024-05-11 11:03:44 +0200

    Documentation comments for Input class

--- src/lib/Engine/Input.ts
+++ src/lib/Engine/Input.ts
@@ -1,4 +1,10 @@
+/**
+ * Engine module for handling input
+ */
 export default class Input {
+	/**
+	 * Stores currently held keys
+	 */
 	private heldKeys: Map<string, boolean> = new Map();
 
 	constructor() {
@@ -6,22 +12,34 @@ export default class Input {
 		window.addEventListener('keyup', this.handleKeyUp.bind(this));
 	}
 
+	/**
+	 * Adds given key to held keys map
+	 */
 	private handleKeyDown(event: KeyboardEvent): void {
 		const key = event.key;
 		if (this.heldKeys.has(key)) return;
 		this.heldKeys.set(key, true);
 	}
 
+	/**
+	 * Removes given key from held keys map
+	 */
 	private handleKeyUp(event: KeyboardEvent): void {
 		const key = event.key;
 		if (!this.heldKeys.has(key)) return;
 		this.heldKeys.delete(key);
 	}
 
+	/**
+	 * Returns true if given key is currently held
+	 */
 	public isKeyHeld(key: string): boolean {
 		return this.heldKeys.has(key);
 	}
 
+	/**
+	 * Removes all held keys from registy
+	 */
 	public reset(): void {
 		this.heldKeys.clear();
 	}
