6b512f37aaabd7af1b268d6f1e2d4bde2b266379
sutaC <catus.pl@gmail.com>
2024-05-13 12:14:05 +0200

    Alternative button controler

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -8,6 +8,14 @@
 	onMount(() => {
 		const canvas = document.querySelector('canvas') as HTMLCanvasElement;
 		game = new Game(canvas);
+
+		game.addAlternativeButton(document.querySelector('#left') as HTMLButtonElement, 'a');
+		game.addAlternativeButton(document.querySelector('#forward') as HTMLButtonElement, 'w');
+		game.addAlternativeButton(document.querySelector('#right') as HTMLButtonElement, 'd');
+		game.addAlternativeButton(document.querySelector('#backward') as HTMLButtonElement, 's');
+		game.addAlternativeButton(document.querySelector('#up') as HTMLButtonElement, 'Shift');
+		game.addAlternativeButton(document.querySelector('#down') as HTMLButtonElement, 'Control');
+
 		game.onready = (gm) => {
 			gm.run();
 			console.log('Game is running!');
@@ -37,9 +45,20 @@
 
 <canvas width="500" height="500"></canvas>
 
-<p class="controlls">
-	W-S : moving forward-backward <br /> A-D : looking left-right <br /> Shift-Ctrl : moving up-down
-</p>
+<div class="wrap">
+	<p class="controlls">
+		W-S : moving forward-backward <br /> A-D : looking left-right <br /> Shift-Ctrl : moving up-down
+	</p>
+
+	<div class="controller">
+		<button id="left" aria-label="Left">&lArr;</button>
+		<button id="forward" aria-label="Forward">&uArr;</button>
+		<button id="right" aria-label="Right">&rArr;</button>
+		<button id="backward" aria-label="Backward">&dArr;</button>
+		<button id="up" aria-label="Up">&uarr;</button>
+		<button id="down" aria-label="Down">&darr;</button>
+	</div>
+</div>
 
 <style>
 	.return {
@@ -53,7 +72,49 @@
 		margin: 0 0 1rem;
 	}
 
+	.wrap {
+		margin: 0.5rem 0;
+		display: flex;
+		justify-content: center;
+		align-items: start;
+		gap: 1rem;
+		padding-inline: 2rem;
+	}
+
+	.controller {
+		display: grid;
+		text-align: center;
+		justify-content: center;
+		gap: 0.125rem;
+		grid-template-areas: '. forward . up' 'left backward right down';
+	}
+
+	.controller > * {
+		aspect-ratio: 1;
+		width: 2.5rem;
+	}
+
+	#left {
+		grid-area: left;
+	}
+	#forward {
+		grid-area: forward;
+	}
+	#right {
+		grid-area: right;
+	}
+	#backward {
+		grid-area: backward;
+	}
+	#up {
+		grid-area: up;
+	}
+	#down {
+		grid-area: down;
+	}
+
 	.controlls {
 		text-align: center;
+		margin: 0;
 	}
 </style>
